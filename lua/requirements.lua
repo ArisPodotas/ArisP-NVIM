d03e57ec20e38046c9d62433bf9f003ec6143f6d
Aris Podotas <a.Podotas@gmail.com>
2024-07-06 10:15:05 +0300

    added the pwer symbol to lualine and made the datetime function in lualine

--- lua/requirements.lua
+++ lua/requirements.lua
@@ -223,17 +223,21 @@ require('lazy').setup({
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', { 'diagnostics', symbols = { error = ' ', warn = ' ', info = ' ', hint = ' '}, sources = { "nvim_diagnostic" } } },
         lualine_c = {'filename'},
-        -- lualine_x = {},
-        -- lualine_x = {"󰁹" .. battery .. "%"},
         lualine_y = {'progress'},
         lualine_z = {'location', 'searchcount', function() local handle = io.popen("wmic path Win32_Battery get EstimatedChargeRemaining")
-                                              local result = handle:read("*a")
-                                              handle:close()
-
-                                              -- Extract the number from the output
-                                              local batteryPercentage = tonumber(result:match("%d+"))
-                                              return batteryPercentage
-                                          end}
+                                                    if handle then
+                                                      local result = handle:read("*a")
+                                                      handle:close()
+                                                      local batteryPercentage = tonumber(result:match("%d+"))
+                                                      return tostring(batteryPercentage) .. " 󰁹"
+                                                    else
+                                                      print("batteryPercentage failed")
+                                                  end
+                                                end,
+                                                function ()
+                                                    local dateTimeString = os.date("%d-%m %H:%M")
+                                                    return dateTimeString
+                                                end }
       },
     },
   },
